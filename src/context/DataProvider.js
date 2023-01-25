58d7206cbb41e6e8cabdc0857495ddc1a084f34d
josebetancourtzam18 <josebetancourtzam18@gmail.com>
2023-01-25 17:32:40 -0400

    agregando servicio

--- src/context/DataProvider.js
+++ src/context/DataProvider.js
@@ -1,68 +1,80 @@
-import React, { createContext, useState, useContext } from "react";
-import { fetchData } from "../helpers/fetch";
-import { AuthContext } from "../auth/AuthContext";
+import React, { createContext, useState, useContext, useEffect } from 'react';
+import { fetchData } from '../helpers/fetch';
+import { AuthContext } from '../auth/AuthContext';
 
 export const DataContext = createContext();
 
 export const DataProvider = ({ children }) => {
-  const [rows, setRows] = useState([]);
-  const [openbd, setOpenbd] = useState(false);
-  const [tipoID, setTipoID] = useState("V");
-  const [openDialog, setOpenDialog] = useState(false);
+	const [rows, setRows] = useState([]);
+	const [openbd, setOpenbd] = useState(false);
+	const [tipoID, setTipoID] = useState('V');
+	const [openDialog, setOpenDialog] = useState(false);
 
-  const {  setOpen, setMsn } = useContext(
-    AuthContext
-  );
-  const fetchInfo = async (tipoDocu, proveedor, ci) => {
-    console.log(tipoDocu);
-    setOpenbd(true);
-    const codProveedor = await fetchData("/ObtieneCodProv", {
-      cProveedor: proveedor,
-    });
-    const respAsegurabilidad = await fetchData("/ConsultaAsegurabilidad", {
-      cCodProv: codProveedor.data[0].CODPROV,
-      cTipoId: tipoDocu,
-      nNumId: parseInt(ci),
-      cDvId: tipoDocu == "M" ? null: "0",//"0",
-    });
+	const { setOpen, setMsn } = useContext(AuthContext);
+	const fetchInfo = async (tipoDocu, proveedor, ci) => {
+		console.log(tipoDocu);
+		setOpenbd(true);
+		const codProveedor = await fetchData('/ObtieneCodProv', {
+			cProveedor: proveedor,
+		});
+		const respAsegurabilidad = await fetchData('/ConsultaAsegurabilidad', {
+			cCodProv: codProveedor.data[0].CODPROV,
+			cTipoId: tipoDocu,
+			nNumId: parseInt(ci),
+			cDvId: tipoDocu == 'M' ? null : '0', //"0",
+		});
 
-    const arrayTabla = await respAsegurabilidad.data.Asegurados_cur.map(
-      (asegurado) => {
-        return {
-          NOMASEG: asegurado.NOMASEG,
-          CEDULA_ASEGURADO: asegurado.CEDULA_ASEGURADO,
-          TOMADOR: asegurado.TOMADOR,
-          DESCPARENTESCO:asegurado.DESCPARENTESCO,
-          COBERTURAS: respAsegurabilidad.data.Aseg_coberturas_cur.filter(
-            (cobertura) =>
-              cobertura.NUMCERT === asegurado.NUMCERT &&
-              cobertura.CEDULA_ASEGURADO === asegurado.CEDULA_ASEGURADO &&
-              cobertura.DVIDASEG === asegurado.DVIDASEG
-          ),
-        };
-      }
-    );
-    
-    await setRows(arrayTabla);
-   
-    setOpenbd(false);
-  };
+		const arrayTabla = await respAsegurabilidad.data.Asegurados_cur.map((asegurado) => {
+			return {
+				NOMASEG: asegurado.NOMASEG,
+				CEDULA_ASEGURADO: asegurado.CEDULA_ASEGURADO,
+				TOMADOR: asegurado.TOMADOR,
+				DESCPARENTESCO: asegurado.DESCPARENTESCO,
+				COBERTURAS: respAsegurabilidad.data.Aseg_coberturas_cur.filter(
+					(cobertura) =>
+						cobertura.NUMCERT === asegurado.NUMCERT &&
+						cobertura.CEDULA_ASEGURADO === asegurado.CEDULA_ASEGURADO &&
+						cobertura.DVIDASEG === asegurado.DVIDASEG
+				),
+			};
+		});
 
-  return (
-    <DataContext.Provider
-      value={{
-        rows,
-        fetchInfo,
-        setRows,
-        setOpenbd,
-        openbd,
-        setOpenDialog,
-        openDialog,
-        setTipoID,
-        tipoID,
-      }}
-    >
-      {children}
-    </DataContext.Provider>
-  );
+		await setRows(arrayTabla);
+
+		setOpenbd(false);
+	};
+
+	const geDataAuto = async () => {
+		const res = await fetchData('devuelve_poliza_auto', {
+			p_tipoid: 'V',
+			p_numid: 11604787,
+			p_dvid: '0',
+			p_numplaca: '',
+			p_serialcarroceria: '',
+		});
+		console.log(res.data.c_polizas);
+	};
+
+	useEffect(() => {
+		geDataAuto();
+	}, []);
+
+	return (
+		<DataContext.Provider
+			value={{
+				rows,
+				fetchInfo,
+				setRows,
+				setOpenbd,
+				openbd,
+				setOpenDialog,
+				openDialog,
+				setTipoID,
+				tipoID,
+				geDataAuto,
+			}}
+		>
+			{children}
+		</DataContext.Provider>
+	);
 };
